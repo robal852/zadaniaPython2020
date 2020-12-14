c7010ed344b25a286a2167b9614a574dee16ad57
Albert <albertsurmacz@gmail.com>
2020-12-14 21:11:40 +0100

    zadanie 10.8 DO UZUPEŁNIENIA

---
+++ Zestaw10/zadanie_10_8.py
@@ -0,0 +1,16 @@
+# Stworzyć ADT w postaci kolejki losowej, z której elementy będą pobierane w losowej kolejności.
+# Zadbać o to, aby każda operacja była wykonywana w stałym czasie, niezależnie od liczby elementów w kolejce.
+
+class RandomQueue:
+
+    def __init__(self): pass
+
+    def insert(self, item): pass
+
+    def remove(self): pass  # zwraca losowy element
+
+    def is_empty(self): pass
+
+    def is_full(self): pass
+
+    def clear(self): pass  # czyszczenie listy
