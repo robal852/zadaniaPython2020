1373681532cf98f603186e796ae9e46177fe9a7e
Albert <albert.surmacz@student.uj.edu.pl>
2021-01-09 11:19:17 +0100

    poprawione: 12.3

--- zestaw12/zadanie_12_3.py
+++ zestaw12/zadanie_12_3.py
@@ -1,7 +1,15 @@
 # ZADANIE 12.3 (MEDIANA)
 # Zaimplementować prostą metodę znajdowania mediany polegającą na posortowaniu zbioru i wybraniu elementu środkowego.
 
-def mediana_sort(L, left, right):
+def mediana_sort(L1, left, right):
+    L = []
+    while left <= right:
+        L.append(L1[left])
+        left += 1
+    print("L", L)
+    left = 0
+    right = len(L) - 1
+
     L.sort()
     if len(L) % 2:
         i = ((left + right) // 2)
@@ -15,3 +23,6 @@ L = [0, 1, 2, 3, 4, 5]
 print(mediana_sort(L, 0, len(L) - 1))
 L = [100, 50]
 print(mediana_sort(L, 0, len(L) - 1))
+
+L = [0, 1, 2, 3, 4, 5]
+print(mediana_sort(L, 1, 3))
